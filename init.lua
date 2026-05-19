5d8c319f32ab803405319ba6938cf1fd7b117be5
beppvis <visruthvijind@gmail.com>
2026-05-19 12:22:43 +0530

    Enabling pony-lsp

--- init.lua
+++ init.lua
@@ -117,6 +117,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+vim.lsp.enable("pony_lsp")
+--
+--
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
